07e21320d4b2efef3cbdf17aee4011b98f37898f
Medlyn Jacob <medjacob.22@gmail.com>
2021-08-22 09:50:23 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [num,setNum]=useState(0);
+
+  const increment=()=>{
+    setNum(num+1)
+  }
+  const decrement=()=>{
+    setNum(num-1)
+  }
+  return(
+  <div>
+      <div id="main_container">
+      <h1>COUNTER</h1>
+      <div id="box">
+      <h2>{num}</h2>
+        <div id="btn_collection">
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
+      </div>
+      
+        <h6>MJ</h6>
+      </div>
+  </div>
+  )
 }
 
 export default App;
